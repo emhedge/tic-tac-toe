e72a1343669b25a8db98f8dc9029ec233a20ba5a
emhedge <122066271+emhedge@users.noreply.github.com>
2026-02-24 15:45:38 +0800

    Created newBoard, cleaned up GameController funcs
    I created a function within Gameboard to reset the
     board to its initial state - filled with 0s.
     Previously this was done once at startup based on
     a simple nested for loop. I created a function
     with this loop so that it can be called upon a
     win or tie condition.
    
    I also cleaned up the GameController functions to
    make it a little cleaner and easier to read. In
    GameController, I created the consts checkWin and
    checkTie, then assigned these to constants within
    playRound. This means the playRound logic is much
    more streamlined and simpler, with callback funcs
    from the GameController scope used instead of
    long, complicated logic.

--- script.js
+++ script.js
@@ -5,28 +5,31 @@ function Gameboard() {
     const columns = 3;
     const board = [];
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
+    const newBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i].push(Cell());
+            }
         }
+        
     }
-
+    
     const getBoard = () => board;
-
+    
     // receives boolean value from setValue(), passes to GameController's playRound
     const placeMark = (row, column, player) => {
         return board[row][column].setValue(player)
     }
-
-
+    
+    
     const printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
         console.table(boardWithCellValues);
     }
-
-
-    return {getBoard, placeMark, printBoard}
+    
+    newBoard();
+    return {newBoard, getBoard, placeMark, printBoard}
 }
 
 function Cell() {
@@ -55,6 +58,8 @@ function GameController(
 ) {
     const board = Gameboard();
 
+    const resetBoard = board.newBoard;
+
     const players = [
         {
             name: playerOneName,
@@ -79,43 +84,57 @@ function GameController(
         console.log(`${getActivePlayer().name}'s turn.`)
     }
     
+    function checkWin() {
+        // win condition checks
+        const isRowWin = board.getBoard().some(row => row.every(cell => cell.getValue() === getActivePlayer().mark))
+        
+        const getCol = board.getBoard()[0].map((_, colIndex) => 
+            board.getBoard().map(row => row[colIndex])
+        );
+        const isColWin = getCol.some(col => col.every(cell => cell.getValue() === getActivePlayer().mark))
+        
+        const isDiagonalWin = board.getBoard().every((row, index) => row[index].getValue() === getActivePlayer().mark);
+        
+        const maxIndex = board.getBoard().length - 1;
+        const isAntiDiagonalWin = board.getBoard().every((row, index) => row[(maxIndex) - index].getValue() === getActivePlayer().mark)
+
+        return isRowWin || isColWin || isDiagonalWin || isAntiDiagonalWin;
+    }
+
+    function checkTie() {
+        // tie condition check
+        const isTie = board.getBoard().every(row => row.every(cell => cell.getValue() !== 0))
+        return isTie;
+    }
+
     const playRound = (row, column) => {
         console.log(
             `Adding ${getActivePlayer().name}'s mark to row ${row}, column ${column}...`
         );
 
         const success = board.placeMark(row, column, getActivePlayer().mark);
+
+        if (!success) {
+            console.log(`That spot is already marked. Pick again.`);
+            return;
+        }
         
-        // win condition checks
-        const isRowWin = board.getBoard().some(row => row.every(cell => cell.getValue() === getActivePlayer().mark))
+        const win = checkWin();
+        const tie = checkTie();
 
-        const getCol = board.getBoard()[0].map((_, colIndex) => 
-            board.getBoard().map(row => row[colIndex])
-        );
-        const isColWin = getCol.some(col => col.every(cell => cell.getValue() === getActivePlayer().mark))
-
-        const isDiagonalWin = board.getBoard().every((row, index) => row[index].getValue() === getActivePlayer().mark);
-
-        const maxIndex = board.getBoard().length - 1;
-        const isAntiDiagonalWin = board.getBoard().every((row, index) => row[(maxIndex) - index].getValue() === getActivePlayer().mark)
-
-        // tie condition check
-        const isTie = board.getBoard().every(row => row.every(cell => cell.getValue() !== 0))
-
-        if (success) {
-            if ((isRowWin === true) || (isColWin === true) || (isDiagonalWin === true) || (isAntiDiagonalWin === true)) {
+        if (win) {
+            printNewRound();
+            console.log("A player has won! Play again?")
+            resetBoard();
+            } else if (tie) {
                 printNewRound();
-                console.log("A player has won! Play again?")
-                } else if (isTie === true) {
-                    printNewRound();
-                    console.log("Looks like a tie. Play again?")
-                } else {
-                    switchPlayerTurn();
-                    printNewRound();
-                }   
-            }  else {
-                console.log(`That spot is already marked. Pick again.`)    
-            }
+                console.log("Looks like a tie. Play again?")
+                resetBoard();
+            } else {
+                switchPlayerTurn();
+                printNewRound();
+            }   
+          
         }
     printNewRound();
 
